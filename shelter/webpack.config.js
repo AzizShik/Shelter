191a50108fa13aeebc0c4102ca36c12b6c0becd3
Abdyl-Aziz <azizshihabov@mail.ru>
2025-01-05 18:59:24 +0300

    feat: add handlebars

--- shelter/webpack.config.js
+++ shelter/webpack.config.js
@@ -1,25 +1,57 @@
 const path = require('path');
 const HtmlBundlerPlugin = require('html-bundler-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = {
   mode: 'development',
+
   output: {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    assetModuleFilename: 'assets/images/[name][ext]',
+    assetModuleFilename: 'assets/images/[name].[contenthash:8][ext]',
   },
+
+  resolve: {
+    alias: {
+      '@scripts': path.join(__dirname, 'src/js'),
+      '@styles': path.join(__dirname, 'src/scss'),
+      '@images': path.join(__dirname, 'src/assets/images'),
+      '@icons': path.join(__dirname, 'src/assets/icons'),
+    },
+  },
+
+  devtool: 'inline-source-map',
 
   plugins: [
     new HtmlBundlerPlugin({
       // specify the entry points for HTML pages (or a template)
       entry: {
-        index: 'src/index.html', // save generated HTML into dist/index.html
+        index: {
+          // output dist/index.html
+          import: './src/views/pages/index.hbs',
+          data: {
+            title: 'Breaking Bad',
+            people: ['Walter White', 'Jesse Pinkman'],
+          },
+        },
       },
+
+      preprocessor: 'handlebars',
+      // define handlebars options
+      preprocessorOptions: {
+        partials: ['src/views/partials'],
+        helpers: {
+          arraySize: (array) => array.length,
+        },
+      },
+
       js: {
-        filename: 'js/[name].js', // JS output filename
+        // JS output filename
+        filename: 'js/[name].[contenthash:8].js',
       },
       css: {
-        filename: 'css/[name].css', // CSS output filename
+        // CSS output filename
+        filename: 'css/[name].[contenthash:8].css',
       },
     }),
   ],
@@ -28,19 +60,99 @@ module.exports = {
     rules: [
       {
         test: /\.s?css$/,
-        use: ['css-loader', 'sass-loader'],
+        use: [
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: { sourceMap: true },
+          },
+        ],
       },
       {
-        test: /\.(ico|png|jp?g)/,
+        test: /\.(png|jpe?g|webp|avif)/,
         type: 'asset/resource',
       },
       {
-        test: /\.svg$/,
+        test: /\.(svg|ico)$/,
         type: 'asset/resource',
         generator: {
-          filename: 'assets/icons/[name].svg',
+          filename: 'assets/icons/[name].[contenthash:8][ext]',
         },
       },
+      {
+        test: /\.woff2?$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[name].[contenthash:8][ext]',
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+
+  optimization: {
+    minimizer: [
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.sharpMinify,
+          options: {
+            encodeOptions: {
+              jpeg: {
+                quality: 90,
+                compressionLevel: 2,
+                effort: 4,
+              },
+              webp: {
+                lossless: true,
+              },
+              avif: {
+                lossless: true,
+              },
+              png: {
+                quality: 90,
+                compressionLevel: 2,
+                effort: 4,
+              },
+            },
+          },
+        },
+
+        generator: [
+          {
+            // You can apply generator using `?as=webp`, you can use any name and provide more options
+            preset: 'webp',
+            implementation: ImageMinimizerPlugin.sharpGenerate,
+            options: {
+              encodeOptions: {
+                webp: {
+                  quality: 95,
+                },
+              },
+            },
+          },
+          {
+            // You can apply generator using `?as=avif`, you can use any name and provide more options
+            preset: 'avif',
+            implementation: ImageMinimizerPlugin.sharpGenerate,
+            options: {
+              encodeOptions: {
+                avif: {
+                  quality: 90,
+                },
+              },
+            },
+          },
+        ],
+      }),
     ],
   },
 
